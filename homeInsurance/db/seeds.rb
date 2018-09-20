50ce4ad7d070fa934ae8bfa49e7f5db3a00cb5e6
DEkeSparta <deke@spartaglobal.com>
2018-09-20 17:11:05 +0100

    Added basic seeding, adjusted layouts for various pages and improved information display.

--- homeInsurance/db/seeds.rb
+++ homeInsurance/db/seeds.rb
@@ -7,3 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 noInsurer = Insurer.create({name: "None", slogan: "I'll keep things safe myself", location: "None"})
+noInsurer.save
+
+user1 = User.create({email: "my@test.com", first_name: "my", last_name: "ohmy", insurer_id: 1, postcode: "notapostcode", house_number: 84})
+user1.insurer_id = noInsurer.id
+user1.save
+
+pen =  Item.create({name: "Pen", description: "Ballpoint, parker", value: 29.99, user_id: 1})
+pen.user_id = user1.id
+pen.save
